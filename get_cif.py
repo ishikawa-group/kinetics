d7e3ce3020e77d85e2de7eeeaeae538770e69830
ISHIKAWA Atsushi <ishikawa.a.ai@m.titech.ac.jp>
2024-11-26 14:54:51 +0900

    update

--- get_cif.py
+++ get_cif.py
@@ -3,21 +3,22 @@ import os
 
 MY_API_KEY = os.environ["MAPI"]
 
-#Get cif files of ABO3 systems
-# Only for stable insulators with band_gap > 2 and spacegroup = Pm-3m
-band_gap_min = 2.0
+# Get cif files of ABO3 systems
+name = "**O3"
+band_gap_min = 2.0  # Only for stable insulators with band_gap > 2
 band_gap_max = None
-sg_symb = "Pm-3m"
+sg_symb = "Pm-3m"   # spacegroup = Pm-3m
 _is_stable = True
 _is_metal = False
 
 # define physical properties/infos you want to obtain
-properties = ['formula_pretty','material_id','structure','symmetry','is_metal','band_gap']
-name = "**O3"
-with MPRester( MY_API_KEY ) as mpr:
-    results = mpr.materials.summary.search(formula=name, band_gap=(band_gap_min, band_gap_max), spacegroup_symbol = sg_symb, is_stable=_is_stable, is_metal=_is_metal, fields=properties)
+properties = ["formula_pretty", "material_id", "structure", "symmetry", "is_metal", "band_gap"]
+with MPRester(MY_API_KEY) as mpr:
+    results = mpr.materials.summary.search(formula=name, band_gap=(band_gap_min, band_gap_max),
+                                           spacegroup_symbol=sg_symb, is_stable=_is_stable, is_metal=_is_metal,
+                                           fields=properties)
 
-#Output
+# Output
 path_output_dir = "./ABO3_cif/"
 if not os.path.exists(path_output_dir):
     os.makedirs(path_output_dir)
@@ -28,4 +29,4 @@ for mat in results:
     print(mat.is_metal, mat.band_gap)
 
     ofile = path_output_dir+mat.material_id+"_"+mat.formula_pretty+".cif"
-    mat.structure.to(filename = ofile)
+    mat.structure.to(filename=ofile)
