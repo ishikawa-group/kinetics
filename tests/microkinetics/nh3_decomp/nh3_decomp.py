5714e6ded7d352fb9241dd4ae66c63f05e726986
ISHIKAWA Atsushi <ishikawa.a.ai@m.titech.ac.jp>
2024-12-25 16:08:41 +0900

    microkinetics

--- tests/microkinetics/nh3_decomp/nh3_decomp.py
+++ tests/microkinetics/nh3_decomp/nh3_decomp.py
@@ -1,16 +1,9 @@
-def get_overpotential_for_cif():
+if __name__ == "__main__":
     import numpy as np
-    import argparse
     from kinetics.microkinetics.get_reaction_energy import get_reaction_energy
     from kinetics.microkinetics.utils import fix_lower_surface
     from ase.visualize import view
     from ase.build import fcc111
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--unique_id", default="0")
-    parser.add_argument("--replace_percent", default=0)
-    args = parser.parse_args()
-    unique_id = args.unique_id
 
     surface = fcc111(symbol="Ni", size=(2, 2, 3), vacuum=8.0)
     surface = fix_lower_surface(surface)
@@ -19,17 +12,6 @@ def get_overpotential_for_cif():
 
     reaction_file = "nh3_decomp.txt"
 
-    # --- DFT calculation
-    # try:
-    deltaEs = get_reaction_energy(reaction_file=reaction_file, surface=surface, calculator="emt",
-                                  verbose=True, dirname=unique_id)
-    # except Exception as e:
-    #    print("DFT calculation failed for this material")
-    #    return None
+    deltaEs = get_reaction_energy(reaction_file=reaction_file, surface=surface, calculator="emt", verbose=True)
 
-    return deltaEs
-
-
-if __name__ == "__main__":
-    deltaEs = get_overpotential_for_cif()
     print("deltaEs:", deltaEs)
